a9da70bae1060d0cccc41d7b13cb705b549205e7
Nico Tromp <nico.tromp@gmail.com>
2017-03-08 13:18:53 +0100

    Adjusted phase name.

--- compiler/src/context_analysis/context_analysis.c
+++ compiler/src/context_analysis/context_analysis.c
@@ -49,7 +49,7 @@ static info *FreeInfo( info *info)
   DBUG_RETURN( info);
 }
 
-node *CCprogram(node *arg_node, info *arg_info) {
+node *CAprogram(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCprogram");
 
     TRAVopt(PROGRAM_DECLARATIONS(arg_node), arg_info);
@@ -58,7 +58,7 @@ node *CCprogram(node *arg_node, info *arg_info) {
 }
 
 
-node *CCdeclarations(node *arg_node, info *arg_info) {
+node *CAdeclarations(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCdeclarations");
 
     TRAVopt(DECLARATIONS_NEXT(arg_node), arg_info);
@@ -68,7 +68,7 @@ node *CCdeclarations(node *arg_node, info *arg_info) {
 }
 
 
-node *CCfundec(node *arg_node, info *arg_info) {
+node *CAfundec(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfundec");
 
     printf("Fundec\n");
@@ -77,7 +77,7 @@ node *CCfundec(node *arg_node, info *arg_info) {
 }
 
 
-node *CCfundef(node *arg_node, info *arg_info) {
+node *CAfundef(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfundef");
 
     printf("Fundef\n");
@@ -88,7 +88,7 @@ node *CCfundef(node *arg_node, info *arg_info) {
 }
 
 
-node *CCfunheader(node *arg_node, info *arg_info) {
+node *CAfunheader(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfunheader");
 
     printf("Funheader\n");
@@ -96,7 +96,7 @@ node *CCfunheader(node *arg_node, info *arg_info) {
     DBUG_RETURN(arg_node);
 }
 
-node *CCfunbody(node *arg_node, info *arg_info) {
+node *CAfunbody(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfunbody");
 
     printf("Funbody\n");
@@ -104,7 +104,7 @@ node *CCfunbody(node *arg_node, info *arg_info) {
     DBUG_RETURN(arg_node);
 }
 
-node *CCglobaldec(node *arg_node, info *arg_info) {
+node *CAglobaldec(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCglobaldec");
 
     printf("Globaldec\n");
@@ -112,7 +112,7 @@ node *CCglobaldec(node *arg_node, info *arg_info) {
     DBUG_RETURN(arg_node);
 }
 
-node *CCglobaldef(node *arg_node, info *arg_info) {
+node *CAglobaldef(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCglobaldef");
 
     printf("Globaldef\n");
@@ -120,194 +120,194 @@ node *CCglobaldef(node *arg_node, info *arg_info) {
     DBUG_RETURN(arg_node);
 }
 
-node *CCint(node *arg_node, info *arg_info) {
+node *CAint(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCint");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCfloat(node *arg_node, info *arg_info) {
+node *CAfloat(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfloat");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCbool(node *arg_node, info *arg_info) {
+node *CAbool(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCbool");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCparams(node *arg_node, info *arg_info) {
+node *CAparams(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCparams");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCparam(node *arg_node, info *arg_info) {
+node *CAparam(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCparam");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCvardecs(node *arg_node, info *arg_info) {
+node *CAvardecs(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCvardecs");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCfuncall(node *arg_node, info *arg_info) {
+node *CAfuncall(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfuncall");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCtypecast(node *arg_node, info *arg_info) {
+node *CAtypecast(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCtypecast");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCassign(node *arg_node, info *arg_info) {
+node *CAassign(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCassign");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCif(node *arg_node, info *arg_info) {
+node *CAif(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCif");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCwhile(node *arg_node, info *arg_info) {
+node *CAwhile(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCwhile");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCdo(node *arg_node, info *arg_info) {
+node *CAdo(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCdo");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCfor(node *arg_node, info *arg_info) {
+node *CAfor(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfor");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCreturn(node *arg_node, info *arg_info) {
+node *CAreturn(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCreturn");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCexprs(node *arg_node, info *arg_info) {
+node *CAexprs(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCexprs");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCarithop(node *arg_node, info *arg_info) {
+node *CAarithop(node *arg_node, info *arg_info) {
    DBUG_ENTER("CCarithop");
 
    DBUG_RETURN(arg_node);
 }
 
-node *CCrelop(node *arg_node, info *arg_info) {
+node *CArelop(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCrelop");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CClogicop(node *arg_node, info *arg_info) {
+node *CAlogicop(node *arg_node, info *arg_info) {
     DBUG_ENTER("CClogicop");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCunop(node *arg_node, info *arg_info) {
+node *CAunop(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCunop");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCvoid(node *arg_node, info *arg_info) {
+node *CAvoid(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCvoid");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCintconst(node *arg_node, info *arg_info) {
+node *CAintconst(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCintconst");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCfloatconst(node *arg_node, info *arg_info) {
+node *CAfloatconst(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCfloatconst");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCboolconst(node *arg_node, info *arg_info) {
+node *CAboolconst(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCboolconst");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCsymboltableentry(node *arg_node, info *arg_info) {
+node *CAsymboltableentry(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCsymboltableentry");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCerror(node *arg_node, info *arg_info) {
+node *CAerror(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCerror");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CClocalfundef(node *arg_node, info *arg_info) {
+node *CAlocalfundef(node *arg_node, info *arg_info) {
     DBUG_ENTER("CClocalfundef");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CClocalfundefs(node *arg_node, info *arg_info) {
+node *CAlocalfundefs(node *arg_node, info *arg_info) {
     DBUG_ENTER("CClocalfundefs");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCarrayassign(node *arg_node, info *arg_info) {
+node *CAarrayassign(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCarrayassign");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCarray(node *arg_node, info *arg_info) {
+node *CAarray(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCarray");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCids(node *arg_node, info *arg_info) {
+node *CAids(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCids");
 
     DBUG_RETURN(arg_node);
 }
 
-node *CCarrexprs(node *arg_node, info *arg_info) {
+node *CAarrexprs(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCarrexprs");
 
     DBUG_RETURN(arg_node);
 }
 
 
-node *CCstatements(node *arg_node, info *arg_info) {
+node *CAstatements(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCstatements");
 
     printf("Statements\n");
@@ -317,7 +317,7 @@ node *CCstatements(node *arg_node, info *arg_info) {
     DBUG_RETURN(arg_node);
 }
 
-node *CCvardec(node *arg_node, info *arg_info) {
+node *CAvardec(node *arg_node, info *arg_info) {
     DBUG_ENTER("CCvardec");
 
     printf("Var Declaration [%s]\n", ID_NAME(VARDEC_ID(arg_node)));
@@ -325,7 +325,7 @@ node *CCvardec(node *arg_node, info *arg_info) {
     DBUG_RETURN(arg_node);
 }
 
-node *CCid(node * arg_node, info * arg_info) {
+node *CAid(node * arg_node, info * arg_info) {
     DBUG_ENTER("CCid");
 
     printf("Var usage [%s]\n", ID_NAME(arg_node));
@@ -333,12 +333,12 @@ node *CCid(node * arg_node, info * arg_info) {
     DBUG_RETURN(arg_node);
 }
 
-node *CCdoScopeAnalysis( node *syntaxtree) {
+node *CAdoScopeAnalysis( node *syntaxtree) {
     DBUG_ENTER("CCdoScopeAnslysis");
 
     info *arg_info = MakeInfo();
 
-    TRAVpush(TR_cc);
+    TRAVpush(TR_ca);
 
     TRAVdo(syntaxtree, NULL);
 
