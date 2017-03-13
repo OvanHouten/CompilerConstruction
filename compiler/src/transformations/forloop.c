22dde9165b8b5c42eeea074bddd0d25c13e6d10f
Nico Tromp <nico.tromp@gmail.com>
2017-03-13 10:12:21 +0100

    Preparing for for-loop transformation.

---
+++ compiler/src/transformations/forloop.c
@@ -0,0 +1,38 @@
+/*
+ * forloop.c
+ *
+ *  Created on: 13 Mar 2017
+ *      Author: nico
+ */
+
+#include "dbug.h"
+
+#include "forloop.h"
+
+node *FLfor(node *arg_node, info *arg_info) {
+    DBUG_ENTER("FLfor");
+
+    DBUG_RETURN(arg_node);
+}
+
+node *FLfundef(node *arg_node, info *arg_info) {
+    DBUG_ENTER("FLfundef");
+
+    DBUG_RETURN(arg_node);
+}
+
+node *FLlocalfundef(node *arg_node, info *arg_info) {
+    DBUG_ENTER("FLlocalfundef");
+
+    DBUG_RETURN(arg_node);
+}
+
+
+node *FLdoForLoop(node *syntaxtree) {
+    DBUG_ENTER("FLdoForloop");
+
+    DBUG_RETURN(syntaxtree);
+}
+
+
+
---
+++ compiler/src/transformations/forloop.h
@@ -0,0 +1,19 @@
+/*
+ * forloo.h
+ *
+ *  Created on: 13 Mar 2017
+ *      Author: nico
+ */
+
+#ifndef SRC_TRANSFORMATIONS_FORLOOP_H_
+#define SRC_TRANSFORMATIONS_FORLOOP_H_
+
+#include "types.h"
+
+extern node *FLfor(node *arg_node, info *arg_info);
+extern node *FLfundef(node *arg_node, info *arg_info);
+extern node *FLlocalfundef(node *arg_node, info *arg_info);
+
+extern node *FLdoForLoop(node *syntaxtree);
+
+#endif /* SRC_TRANSFORMATIONS_FORLOOP_H_ */
